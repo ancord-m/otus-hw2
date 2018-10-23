e8627075699def8df9ce528a278b1bb2f09b7585
Michael Latushko <ancord.m@yandex.ru>
2018-10-23 11:54:13 +0300

    Draft of the test module was added. CMakeLists was edited. All work

---
+++ ip_filter_tests.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE Tests
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(Group)
+
+BOOST_AUTO_TEST_CASE(StartHere)
+{
+
+	BOOST_CHECK(1 > 0);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
