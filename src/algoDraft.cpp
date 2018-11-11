067e3790e17367e119da2034ce8a35063a1be6bd
Michael Latushko <ancord.m@yandex.ru>
2018-11-11 14:32:50 +0300

    Inverse sort Algorithm found

--- src/algoDraft.cpp
+++ src/algoDraft.cpp
@@ -8,28 +8,34 @@ using namespace std;
 
 bool ipComparator(std::vector<std::string> a, std::vector<std::string> b)
 {
-	bool a_goes_before_b = false;	
-	
+	bool a_goes_before_b = false;
+	unsigned int numA, numB;
 	//for(int i = a.size() - 1; i >= 0; i--) {
 	for(int i = 0; i < a.size(); ++i) {
-		std::cout << a[i] << " " << b[i] << std::endl;
-	  if(stoul(a[i], nullptr, 10) < stoul(b[i], nullptr, 10))  {
+//		std::cout << a[i] << " " << b[i] << std::endl;
+		numA = stoul(a[i], nullptr, 10);
+		numB = stoul(b[i], nullptr, 10);
+	  if(numA == numB) continue;
+
+	  if( numA > numB)  {
 	//	if(a[i] > b[i]) {
 		  a_goes_before_b = true;
 		  break;
-		}/* else {
+		} else if(numA < numB){
 			a_goes_before_b = false;
-		}*/
+			break;
+		}
+
 	}
-	
+
 	return a_goes_before_b;
 }
- 
+
 int main()
-{   
+{
 	std::vector<std::string> temp;
-	
-	/*std::vector<std::vector<int> > pool = 
+
+	/*std::vector<std::vector<int> > pool =
 				{
 						{ 222, 173, 235 },
 						{ 222, 130, 177 },
@@ -38,33 +44,33 @@ int main()
 						{ 99, 100, 168 },
 						{ 100, 2, 234 }
 				};*/
-	std::vector<std::vector<std::string> > pool = 
-			{
-					{ "85", "254", "10", "197" },	
-					{ "23", "240", "215", "189" },
-					{ "1",  "29", "168", "152" },
-					{ "185", "69", "186", "168" },
-					{ "110", "152", "103", "161" },
-					{ "68", "45", "152", "82" },
-					{ "185", "182", "81", "131" },
-					{ "114", "221", "179", "117" },
-					{ "185", "89", "100", "249" },
-					{ "46", "70", "113", "73" },
-					{ "185", "68", "0", "38" }
-			};
+	std::vector<std::vector<std::string> > pool =
+				{
+						{ "85", "254", "10", "197" },
+						{ "23", "240", "215", "189" },
+						{ "1",  "29", "168", "152" },
+						{ "185", "69", "186", "168" },
+						{ "110", "152", "103", "161" },
+						{ "68", "45", "152", "82" },
+						{ "185", "182", "81", "131" },
+						{ "114", "221", "179", "117" },
+						{ "185", "89", "100", "249" },
+						{ "46", "70", "113", "73" },
+						{ "185", "68", "0", "38" }
+				};
 
-	
+
 	for(auto i: pool) {
-		std::cout << std::endl;		
+		std::cout << std::endl;
 		for(auto j: i) {
 			if(j != i[0] ) std::cout << ".";
 			std::cout << j;
 		}
 	}
-	
-	
+
+
 	std::cout << "\n\nAfter sort " << std::endl;
-	
+
 	std::sort(pool.begin(), pool.end(), ipComparator);
 
 	for(auto i: pool) {
@@ -74,6 +80,7 @@ int main()
 			std::cout << j;
 		}
 	}
-	
+
     return 0;
 }
+
